5826242d1caa03f409a69b8491915d9bb403cc46
PuraCaseus <94827270+PuraCaseus@users.noreply.github.com>
2022-05-04 10:13:37 +0200

    Game character and animations

--- Assets/Scripts/Smack.cs
+++ Assets/Scripts/Smack.cs
@@ -2,17 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Whack : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Smack : MonoBehaviour {
+
+    public LayerMask enemyLayers;
+    private Animator anim;
+    public Transform kickPoint;
+
+    public float kickRange = 0.5f;
+    public int attackDamage = 10;
+
+    public float kickRate = 2f;
+    float nextKickTime = 0f;
+
+    [SerializeField] private AudioSource LlamaKick;
+    [SerializeField] private AudioSource ManHurt;
+
+    void Start(){
+        anim = GetComponent<Animator>();
+    }
+    
+    void Update(){
+
+        if(Time.time >= nextKickTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Kick();
+                nextKickTime = Time.time + 1f / kickRate;
+            }
+        }  
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Kick(){
+
+        anim.SetTrigger("Whack");
+        LlamaKick.Play();
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(kickPoint.position, kickRange, enemyLayers);
         
+        //foreach(Collider2D enemy in hitEnemies)
+        {
+            ///print("we hit" + enemy.name);
+            //enemy.GetComponent<Damageable>().TakeDamage(attackDamage);
+        }
     }
+    private void OnDrawGizmosSelected()
+    {
+        if (kickPoint == null)
+            return;
+        Gizmos.DrawWireSphere(kickPoint.position, kickRange);
+    }
+
 }
