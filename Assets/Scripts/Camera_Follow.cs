19086ad6118b227f2d694ddbf9046157b4c5a100
PuraCaseus <94827270+PuraCaseus@users.noreply.github.com>
2022-05-05 10:42:44 +0200

    Camera rework

--- Assets/Scripts/Camera_Follow.cs
+++ Assets/Scripts/Camera_Follow.cs
@@ -1,20 +1,18 @@
 using UnityEngine;
 using System.Collections;
-public class Camera_Follow : MonoBehaviour
-{
-public GameObject player;
-private Vector3 offset;
-void Start ()
-{
-    offset = transform.position - player.transform.position;
-}
-void LateUpdate ()
-{
-transform.position = player.transform.position + offset;
-}
-}
 
-    
-        
-          
-   
+public class Camera_Follow: MonoBehaviour
+{
+
+    public Transform Player;
+    public float DampTime = 0.4f;
+    private Vector3 cameraPos;
+    private Vector3 velocity = Vector3.zero;
+
+    void FixedUpdate()
+    {
+        cameraPos = new Vector3(Player.position.x, Player.position.y, -10f);
+        transform.position = Vector3.SmoothDamp(gameObject.transform.position, cameraPos, ref velocity, DampTime);
+    }
+
+}
