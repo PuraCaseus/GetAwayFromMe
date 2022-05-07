10434cfebe216cd9c82299ff1f02d6edc92c2e88
PuraCaseus <94827270+PuraCaseus@users.noreply.github.com>
2022-05-07 13:18:13 +0200

    Trying to flip player

---
+++ Assets/Scripts/FlipPlayer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipPlayer : MonoBehaviour
+{
+    public GameObject pivot;
+
+    public Aiming aimScript;
+
+    void Start()
+    {
+        //Reference to the Aiming script
+        aimScript = pivot.GetComponent<Aiming>();
+    }
+
+    void FixedUpdate()
+    {
+        //Flip player
+        if (aimScript.lookDirection < -90 || aimScript.lookDirection > 90)
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+
+}
