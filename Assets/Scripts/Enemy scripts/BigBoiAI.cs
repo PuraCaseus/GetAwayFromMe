13e2194252839be716c41611511fbbcac2546c98
PuraCaseus <94827270+PuraCaseus@users.noreply.github.com>
2022-05-10 12:44:53 +0200

    Boss rework!
    Boss now has an actual attack.
    iframe dodge is needed though

--- Assets/Scripts/Enemy scripts/BigBoiAI.cs
+++ Assets/Scripts/Enemy scripts/BigBoiAI.cs
@@ -7,25 +7,58 @@ public class BigBoiAI : MonoBehaviour
     public float speed;
     public Transform target;
     public float minimumDistance;
-    public LayerMask enemy;
-    public int attackDamage = 10;
+
+    [SerializeField]
+    float aggroRange;
+
+    Vector2 movement;
+    public Animator anim;
+
+
+
     
     void Update()
     {
-        if (Vector2.Distance(transform.position, target.position) > minimumDistance)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-        } else
-        {
-            //enemy.GetComponent<EnemyAttack>().TakeDamage(attackDamage);
+      float DistToTarget = Vector2.Distance(transform.position, target.position);
+      if(Vector2.Distance(transform.position, target.position) > minimumDistance)
+      {
+        anim.SetBool("isRunning", true);
+        ChaseTarget();
+      }
+      else
+      {
+        anim.SetTrigger("isAttacking");
+      }
 
+      
+
+
+    }
+
+    void ChaseTarget()
+    {
+     if(transform.position.x < target.position.x)
+      {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        transform.eulerAngles = new Vector3(0, -180, 0);
+      }
+      else
+      {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        transform.eulerAngles = new Vector3(0, 0, 0);
+      }
+    }
+
+
+    void StopChasingTarget()
+    {
+        float DistToTarget = Vector2.Distance(transform.position, target.position);
+        if(DistToTarget < aggroRange)
+        {
+            anim.SetBool("isRunning", false);
+         
         }
 
-    
-
-
-
-
-    } 
-    
+    }
 }
+
